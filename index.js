05d6f62bc1786496cfac13b15601ce2d08ef4d03
EncrypterParv <parvgoyal1321@gmail.com>
2025-12-06 02:44:29 +0530

    Added add to cart notification function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const fadeElements= document.querySelectorAll(".fade-in")
 function handleScroll(){
     fadeElements.forEach(el=>{
@@ -12,3 +14,23 @@ function handleScroll(){
 }
 window.addEventListener("scroll", handleScroll);
 window.addEventListener("load", handleScroll);
+document.querySelectorAll(".add-cart").forEach(e=>{
+    e.addEventListener("click", ()=>{
+        showcartpopup();
+    })
+})
+function showcartpopup(){
+    const popup= document.createElement("div");
+    popup.className= "cart-popup";
+    popup.innerText= "Added To Cart !";
+    document.body.appendChild(popup)
+
+    setTimeout(()=>{
+        popup.style.opacity= "0";
+        popup.style.transform= "translateY(20px)";
+    }, 1400);
+
+    setTimeout(() => {
+        popup.remove()
+    }, 2000);
+}
